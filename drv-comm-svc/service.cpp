1da0f1d46c7e330f525781daa91fc2fe18b722e6
Olga Fomina <o.fomina963@outlook.com>
2026-04-04 16:31:21 +0300

    + check registry after notification, tested

--- drv-comm-svc/service.cpp
+++ drv-comm-svc/service.cpp
@@ -1,9 +1,19 @@
 #include "service.hpp"
+#include "driver-session/driver-session.hpp"
+#include "registry-run-monitor/registry-run-mon.hpp"
 
 // Точка входа в службу
 int __cdecl _tmain(int argc, TCHAR *argv[])
 {
-    std::cout << "Hello" << std::endl;
+    RegistryRunMonitor monitor(L"C:\\monitoring\\run_snapshot.dat");
+    
+    // Первый запуск: просто сохраняем текущее состояние как "базовое"
+    monitor.CheckAndUpdate();
+    monitor.SaveSnapshot();
+
+    DriverSession drv_session;
+
+    // Если получили уведомление от DriverSession, то получаем данные о новых записях и кладем их в бд
 
     return 0;
 }
